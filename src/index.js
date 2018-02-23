80def307294e21b72e20a9fc2c2d7d54e1c4bd26
aytsaN <aytsan.shamal@gmail.com>
2018-02-23 14:38:35 +0300

    SOLUTION

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    var tempStr='';
+
+    for (let i = 0; i < bracketsConfig.length; ) {
+
+        if (bracketsConfig[i][0] == bracketsConfig[i][1]) {
+            if (str[0] == bracketsConfig[i][0]) {
+                if (tempStr[tempStr.length - 1] == bracketsConfig[i][0]) {
+                    tempStr = tempStr.substring(0, tempStr.length - 1)
+                    str = str.substr(1);
+                    if (str == '' && tempStr == '') {
+                        return true;
+                    }
+                    i=0;
+                }
+                else {
+                    tempStr += str[0];
+                    str = str.substr(1);
+                    if (str.length == 0) {
+                        return false;
+                    }
+                    i = 0;
+                }
+            }
+
+        }
+
+            if (str[0] == bracketsConfig[i][0]&&str[0]!=bracketsConfig[i][1]) {
+                tempStr += str[0];
+                str = str.substr(1);
+                if (str.length == 0) {
+                    return false;
+                }
+                i = 0;
+            }
+
+            else if (str[0] == bracketsConfig[i][1]) {
+                if (tempStr[tempStr.length - 1] == bracketsConfig[i][0]) {
+                    tempStr = tempStr.substring(0, tempStr.length - 1);
+                    str = str.substr(1);
+                    if (str.length == 0 && tempStr.length == 0) {
+                        return true;
+                    }
+                    i = 0;
+                }
+                else {
+                    return false;
+                }
+            }
+            else {
+                i++;
+            }
+    }
+        return true;
 }
